ab940f863ac0d3114d7d176a34e71842967aeb9e
Wren Purcell <purcell856@gmail.com>
2023-12-05 13:56:31 -0500

    used promise.all

--- api-call.js
+++ api-call.js
@@ -1,15 +1,18 @@
 import axios from "axios";
 
-export async function getBerries(){
-  const response = await axios.get("https://pokeapi.co/api/v2/berry-flavor/1/")
-  return response.data
+export async function getBerries() {
+  try {
+    const response = await axios.get(
+      "https://pokeapi.co/api/v2/berry-flavor/1/"
+    );
+    if (response.status !== 200) {
+      throw new Error();
+    }
+    return response.data
+  } catch (err) {
+    console.error("Uh oh! " + err);
+  }
 }
-
-
-
-
-
-
 
 // export async function getData(url){
 //   const response = await axios.get(url)
--- main.js
+++ main.js
@@ -1,23 +1,16 @@
 import './style.css'
 import {getBerries} from "./api-call.js"
+import axios from 'axios'
 
 const berryData = await getBerries()
 const berriesArray = await berryData.berries
-console.log(berriesArray)
+// console.log(berriesArray)
 
+const arrayOfpromises = berriesArray.map(async (berryObj)=>{
+  const response =await axios.get(berryObj.berry.url)
+  return response 
+})
 
-  
+const newArrayOfBerryData = await Promise.all(arrayOfpromises)
+console.log(newArrayOfBerryData)
 
-
-
-
-
-
-
-
-
-
-// const arrayOfpromises = berriesArray.map((berryObj)=>{
-//   const response = 
-//   return berryObj.berry.url
-// })
