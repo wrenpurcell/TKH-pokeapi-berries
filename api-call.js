f864920144e6fc562f2211510e4d470a72199c7c
Wren Purcell <purcell856@gmail.com>
2023-12-05 13:13:41 -0500

    commented out un-needed code for step 1

--- api-call.js
+++ api-call.js
@@ -4,7 +4,14 @@ export async function getBerries(){
   const response = await axios.get("https://pokeapi.co/api/v2/berry-flavor/1/")
   return response.data
 }
-export async function getData(url){
-  const response = await axios.get(url)
-  return response.data
-}
+
+
+
+
+
+
+
+// export async function getData(url){
+//   const response = await axios.get(url)
+//   return response.data
+// }
